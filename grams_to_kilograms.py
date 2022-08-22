2f1428cf6609a65c011244d06dc43d0c015469ca
IO <60049721+IO-08@users.noreply.github.com>
2022-08-22 11:27:03 -0700

    Add files via upload

---
+++ grams_to_kilograms.py
@@ -0,0 +1,16 @@
+def convert_to_kilograms(x):
+    kg = (x / 1000)
+    return kg
+def convert_to_grams(y):
+    g = (y *1000)
+    return g
+options= int(input("Would you like like to convert to Kilograms (KG) (1) or Grams (2): "))
+if options == 1:
+    num = int ( input ( "Enter the number to convert to Kilograms :" ) )
+    result = convert_to_kilograms ( num )
+    print ( str ( num ) + " g is " + str ( result ) + " kg" )
+
+elif options == 2:
+    num = int(input("Enter the number to convert Grams:"))
+    result = convert_to_grams(num)
+    print(str(num) + " Kg is " + str(result) + " g")
