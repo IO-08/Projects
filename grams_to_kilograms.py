765c716cd50d09ac28569058519c797a7df5783f
IO <60049721+IO-08@users.noreply.github.com>
2022-08-22 11:26:34 -0700

    Delete grams_to_kilograms.py

--- grams_to_kilograms.py
+++
@@ -1,8 +0,0 @@
-def convert_to_kilograms ( x ) :
-    value = (x / 1000)
-    return value
-
-
-num = int ( input ( "Enter a number to convert:" ) )
-result = convert_to_kilograms ( num )
-print ( str ( num ) + " g is " + str ( result ) + " kg" )
