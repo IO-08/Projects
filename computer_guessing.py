a057f0a09fa31306631eb880f7054f084ec9121f
IO <60049721+IO-08@users.noreply.github.com>
2022-09-12 12:25:31 -0700

    Add files via upload

---
+++ computer_guessing.py
@@ -0,0 +1,22 @@
+import random
+
+def computer_guess ( x ) :
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high :
+            guess = random.randint ( low, high )
+        else :
+            guess = low  # could be low as well
+        feedback = input ( f'Is {guess} too High (H), too low (L), is correct (C) ??' ).lower
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+
+    print (f'yah the computer {guess} is the correct number')
+
+
+computer_guess (10)
