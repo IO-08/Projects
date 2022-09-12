a9115456d95236001086d3c861b61c817ba3e188
IO <60049721+IO-08@users.noreply.github.com>
2022-09-12 10:34:49 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+# Making a Madlib code using the Mad libs worksheet
+#A day at the zoo
+
+word1= input ("Enter an adjective :")
+word2 = input ("Enter a noun:")
+word3 = input ("Enter a verb,past tense:")
+word4 = input ("Enter an adverb:")
+word5 = input ("Enter an adjective :")
+word6 = input ("Enter a noun:")
+word7 = input ("Enter a noun:")
+word8 = input ("Enter a adjective :")
+word9 = input ("Enter a verb:")
+word10 =input("Enter a adverb:")
+word11 = input ("Enter a verb, past tense:")
+word12 = input ("Enter a adjective :")
+
+#print (f"the princesss and the  {word1} if my favorite  {word2} of all time ")
+result =  (f"Today I went to the zoo. I saw a(n) {word1},{word2} jumping up and down in its tree.\
+He {word3}(verb, past tense){word4}adverb)\
+through the large tunnel that led to its {word5}(adjective)\
+{word6}(noun). I got some peanuts and passed\
+them through the cage to a gigantic gray {word7}(noun)\
+towering above my head. Feeding that animal made\
+me hungry. I went to get a {word8}(adjective) scoop\
+of ice cream. It filled my stomach. Afterwards I had to\
+{word9}(verb){word10}(adverb) to catch our bus.\
+When I got home I {word11}(verb, past tense) my\
+mom for a {word12}(adjective) day at the zoo")
+print (result)
